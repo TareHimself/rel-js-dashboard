f1ed0a2e0ebbb3bceb2c3a63ca35aba85d78ac54
TareGit <Tareebelo4@gmail.com>
2021-12-09 09:17:18 -0500

    guild item rework

--- src/components/GuidItem.jsx
+++ src/components/GuidItem.jsx
@@ -5,14 +5,13 @@ function GuildItem(props) {
 
   console.log(props.guild);
     const icon = (props.guild.icon !== null && props.guild.icon !== undefined ) ? `https://cdn.discordapp.com/icons/${props.guild.id}/${props.guild.icon}.png` : 'https://www.howtogeek.com/wp-content/uploads/2021/07/Discord-Logo-Lede.png';
-    const splash = (props.guild.splash !== null && props.guild.splash !== undefined) ? `https://cdn.discordapp.com/splashes/${props.guild.id}/${props.guild.splash}.png` : 'https://www.howtogeek.com/wp-content/uploads/2021/07/Discord-Logo-Lede.png'
 
     const dashboardLink = props.guild.botShard !== undefined ? `/Dashboard?guild=${props.guild.id}` : "/invite";
   return (
     <div className='guild-item'>
         <h3>{props.guild.name}</h3>
         <div className="guild-item-icon">
-        <img src={splash} className="icon-background" alt="Server Icon"/>
+        <img src={icon} className="icon-background" alt="Server Icon"/>
         <img src={icon} className="icon-forground" alt="Server Banner"/>
         </div>
         {props.guild.botShard !== undefined ? <Link className='button' to={dashboardLink} >Dashboard</Link> : <Link className='button' target="_blank" to={dashboardLink} >Invite</Link>}
