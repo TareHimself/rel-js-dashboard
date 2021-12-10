1168becba1958c78e4832d3bf82cf9402d45d760
TareGit <Tareebelo4@gmail.com>
2021-12-10 06:35:36 -0500

    logout functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -13,7 +13,7 @@ import axios from 'axios';
 
 const authURL = "https://discord.com/api/oauth2/authorize?client_id=804165876362117141&redirect_uri=http%3A%2F%2Fumeko.dev%2Fauth&response_type=code&scope=guilds%20identify";
 function User() {
-    const { sessionId, setSessionId,serverLink } = useContext(GlobalAppContext);
+    const { sessionId, setSessionId, serverLink } = useContext(GlobalAppContext);
 
     const [userAvatar, setUserAvatar] = useState('');
 
@@ -26,7 +26,7 @@ function User() {
         }
 
         const headers = { sessionId: sessionId }
-    
+
         axios.get(`${serverLink}/user`, { headers: headers })
             .then((response) => {
                 const data = response.data;
@@ -37,7 +37,6 @@ function User() {
                 else {
                     const extension = data.avatar.startsWith("a_") ? 'gif' : 'png';
                     setUserAvatar(`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.${extension}`);
-                    console.log(data);
                 }
 
             }, (error) => {
@@ -45,10 +44,25 @@ function User() {
                 console.log(error);
             });
 
-    }, [sessionId, setSessionId, userAvatar,serverLink]);
+    }, [sessionId, setSessionId, userAvatar, serverLink]);
 
     function onLogout(clickEvent) {
-        
+        const headers = { sessionId: sessionId }
+
+        axios.post(`${serverLink}/destroy-session`, { headers: headers })
+            .then((response) => {
+                const data = response.data;
+                if (data.result === 'error') {
+                    setSessionId('');
+                    console.log(data.error);
+                }
+                else {
+                    setSessionId('');
+                }
+
+            }, (error) => {
+                console.log(error);
+            });
     }
 
     useEffect(() => {
@@ -56,7 +70,7 @@ function User() {
             const dropdown = document.getElementById('user-menu-dropdown');
             const bounds = dropdown.getBoundingClientRect();
 
-            if((clickEvent.pageX > bounds.left && clickEvent.pageX < bounds.right) && (clickEvent.pageY > bounds.top && clickEvent.pageY < bounds.bottom)) return;
+            if ((clickEvent.pageX > bounds.left && clickEvent.pageX < bounds.right) && (clickEvent.pageY > bounds.top && clickEvent.pageY < bounds.bottom)) return;
 
             setShowMenu(false);
         }
@@ -64,8 +78,7 @@ function User() {
         if (showMenu) {
             window.addEventListener('click', decideCloseMenu);
         }
-        else
-        {
+        else {
             window.removeEventListener('click', decideCloseMenu)
         }
 
@@ -77,15 +90,15 @@ function User() {
         if (userAvatar !== '') {
             return (
                 <div className='user-dropdown'>
-                    <img className='user-avatar' src={userAvatar} alt='avatar' onClick={()=> setShowMenu(true)}/>
-                    {showMenu && 
-                    <div id= 'user-menu-dropdown' className='user-dropdown-content'>
-                    <Link className='dropdown-button' to="/">Home</Link>
-                        <Link className='dropdown-button' to="/servers">Servers</Link>
-                        <Link className='dropdown-button' to="/commands">Commands</Link>
-                        <Link className='dropdown-button' to="/support">Support</Link>
-                        <button className='dropdown-button' onClick={onLogout} >log Out</button>
-                    </div>}
+                    <img className='user-avatar' src={userAvatar} alt='avatar' onClick={() => setShowMenu(true)} />
+                    {showMenu &&
+                        <div id='user-menu-dropdown' className='user-dropdown-content'>
+                            <Link className='dropdown-button' to="/">Home</Link>
+                            <Link className='dropdown-button' to="/servers">Servers</Link>
+                            <Link className='dropdown-button' to="/commands">Commands</Link>
+                            <Link className='dropdown-button' to="/support">Support</Link>
+                            <button className='dropdown-button' onClick={onLogout} >log Out</button>
+                        </div>}
                 </div>
             );
         }
---
+++ src/routes/Support.jsx
@@ -0,0 +1,15 @@
+
+import '../scss/main.scss';
+
+
+function Support() {
+
+  return (
+    <section className='standard-page'>
+      <h1>Coming Soon</h1>
+    </section>
+  );
+
+}
+
+export default Support;
