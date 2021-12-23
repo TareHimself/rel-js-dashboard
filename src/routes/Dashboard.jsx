710e72cc0c099c30ac9fcbdd4b6ad83c13d8d9ce
TareGit <Tareebelo4@gmail.com>
2021-12-23 00:46:51 -0500

    Dashboard updates

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,14 +1,54 @@
 
 import '../scss/main.scss';
-
+import { HiOutlineMenu } from 'react-icons/hi';
+import { GoGraph } from 'react-icons/go';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { SiMonkeytie } from 'react-icons/si'
+import { AiOutlineBug } from 'react-icons/ai'
 
 function Dashboard() {
-  
+
 
   return (
     <section className='standard-page' id='Dashboard'>
-      
-    <h1>Under Construction</h1>
+
+      <div className="dashboard-sidebar">
+
+        <div className="dashboard-sidebar-button">
+          <div className="dashboard-sidebar-button-items">
+            <HiOutlineMenu className='dashboard-sidebar-icon' /><h3>General</h3>
+          </div>
+        </div>
+
+        <div className="dashboard-sidebar-button">
+          <div className="dashboard-sidebar-button-items">
+            <GoGraph className='dashboard-sidebar-icon' /><h3>Analytics</h3>
+          </div>
+        </div>
+
+        <div className="dashboard-sidebar-button">
+          <div className="dashboard-sidebar-button-items">
+            <IoSettingsOutline className='dashboard-sidebar-icon' /><h3>Settings</h3>
+          </div>
+        </div>
+
+        <div className="dashboard-sidebar-button">
+          <div className="dashboard-sidebar-button-items">
+            <SiMonkeytie className='dashboard-sidebar-icon' /><h3>Permissions</h3>
+          </div>
+        </div>
+
+        <div className="dashboard-sidebar-button">
+          <div className="dashboard-sidebar-button-items">
+            <AiOutlineBug className='dashboard-sidebar-icon' /><h3>Bug Submissions</h3>
+          </div>
+        </div>
+
+      </div>
+      <div className="dashboard-content">
+        <h1>Under Construction</h1>
+      </div>
+
     </section>
   );
 
